b408c013d9f0383b1a104f64ed3f384053df36fa
RCBryank <47090766+RCBryank@users.noreply.github.com>
2021-11-23 21:59:28 -0600

    Inicial

---
+++ Programa del Clima/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Programa_del_Clima
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_Check = new System.Windows.Forms.Button();
+            this.CB_City = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.Lbl_Act = new MetroFramework.Controls.MetroLabel();
+            this.Lbl_Max = new MetroFramework.Controls.MetroLabel();
+            this.Lbl_Min = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // BTN_Check
+            // 
+            this.BTN_Check.Location = new System.Drawing.Point(122, 179);
+            this.BTN_Check.Name = "BTN_Check";
+            this.BTN_Check.Size = new System.Drawing.Size(139, 21);
+            this.BTN_Check.TabIndex = 0;
+            this.BTN_Check.Text = "Revisa el Clima";
+            this.BTN_Check.UseVisualStyleBackColor = true;
+            this.BTN_Check.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CB_City
+            // 
+            this.CB_City.FormattingEnabled = true;
+            this.CB_City.Location = new System.Drawing.Point(122, 97);
+            this.CB_City.Name = "CB_City";
+            this.CB_City.Size = new System.Drawing.Size(325, 21);
+            this.CB_City.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Selecciona una ciudad";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(125, 251);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(53, 19);
+            this.metroLabel1.TabIndex = 3;
+            this.metroLabel1.Text = "Minima";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(267, 251);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(56, 19);
+            this.metroLabel2.TabIndex = 4;
+            this.metroLabel2.Text = "Maxima";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(431, 251);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(45, 19);
+            this.metroLabel3.TabIndex = 5;
+            this.metroLabel3.Text = "Actual";
+            // 
+            // Lbl_Act
+            // 
+            this.Lbl_Act.AutoSize = true;
+            this.Lbl_Act.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.Lbl_Act.Location = new System.Drawing.Point(441, 288);
+            this.Lbl_Act.Name = "Lbl_Act";
+            this.Lbl_Act.Size = new System.Drawing.Size(37, 25);
+            this.Lbl_Act.TabIndex = 8;
+            this.Lbl_Act.Text = "00°";
+            // 
+            // Lbl_Max
+            // 
+            this.Lbl_Max.AutoSize = true;
+            this.Lbl_Max.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.Lbl_Max.Location = new System.Drawing.Point(277, 288);
+            this.Lbl_Max.Name = "Lbl_Max";
+            this.Lbl_Max.Size = new System.Drawing.Size(37, 25);
+            this.Lbl_Max.TabIndex = 7;
+            this.Lbl_Max.Text = "00°";
+            // 
+            // Lbl_Min
+            // 
+            this.Lbl_Min.AutoSize = true;
+            this.Lbl_Min.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.Lbl_Min.Location = new System.Drawing.Point(135, 288);
+            this.Lbl_Min.Name = "Lbl_Min";
+            this.Lbl_Min.Size = new System.Drawing.Size(37, 25);
+            this.Lbl_Min.TabIndex = 6;
+            this.Lbl_Min.Text = "00°";
+            this.Lbl_Min.Click += new System.EventHandler(this.Lbl_Min_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Lbl_Act);
+            this.Controls.Add(this.Lbl_Max);
+            this.Controls.Add(this.Lbl_Min);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CB_City);
+            this.Controls.Add(this.BTN_Check);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_Check;
+        private System.Windows.Forms.ComboBox CB_City;
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel Lbl_Act;
+        private MetroFramework.Controls.MetroLabel Lbl_Max;
+        private MetroFramework.Controls.MetroLabel Lbl_Min;
+    }
+}
+
---
+++ Programa del Clima/Form1.cs
@@ -0,0 +1,113 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+using Newtonsoft.Json;
+
+namespace Programa_del_Clima
+{
+
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            RunAsync();
+            FillCBCity(Cities);
+            CB_City.SelectedIndex = 0;
+
+        }
+
+        string[] Cities = { "London", "Monterrey" };
+
+        static HttpClient client = new HttpClient();
+
+        static async Task RunAsync()
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f9c530fc04bca6018e7f703777604a85");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+
+
+        void FillCBCity(string[] _cities)
+        {
+            for(int i = 0; i < _cities.Length; i++)
+            {
+                CB_City.Items.Add(_cities[i]);
+            }
+        }
+
+
+        static async Task<City_Weather> GetWeather(string path)
+        {
+            City_Weather _weather = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                string _sweather = await response.Content.ReadAsStringAsync();
+                JObject _obj = JObject.Parse(_sweather);
+                _weather = JsonConvert.DeserializeObject<City_Weather>(_sweather);
+                
+            }
+            return _weather;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // GetWeather("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=f9c530fc04bca6018e7f703777604a85");
+            string _citie = "https://api.openweathermap.org/data/2.5/weather?q=" + CB_City.Text + "&appid=f9c530fc04bca6018e7f703777604a85&units=metric&lang=sp";
+            City_Weather _weather = Task.Run(()=> GetWeather(_citie)).Result;
+            //var result = Task.Run(() => asyncGetValue()).Result;
+
+            Lbl_Min.Text = _weather.Main.temp_min.ToString() + "°";
+            Lbl_Max.Text = _weather.Main.temp_max.ToString() + "°";
+            Lbl_Act.Text = _weather.Main.temp.ToString() + "°";
+        }
+
+        private void Lbl_Min_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    public class City_Weather
+    {
+        public int ID { get; set; }
+        public string CityName { get; set; }
+        public main Main { get; set; }
+
+        public City_Weather()
+        {
+            ID = 0;
+            CityName = "City";
+            Main = new main();
+        }
+
+        public struct main
+        {
+            public float temp { get; set; }
+            public float feels_like { get; set; }
+            public float temp_min { get; set; }
+            public float temp_max { get; set; }
+            public float pressure { get; set; }
+            public float humidity { get; set; }
+        }
+
+    }
+}
